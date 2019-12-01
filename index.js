bcb7b6f0a22e1b1700a1a8b30715dcc542db8d16
Tenent Jason <15648200+tenent007963@users.noreply.github.com>
2019-12-01 21:12:33 +0800

    Initial commit
    Initial commit

---
+++ index.js
@@ -0,0 +1,90 @@
+// Import packages
+var fs = require('fs'); 
+var http=require('http');
+var path = require("path");
+var url=require('url');
+var PptxGenJS = require("pptxgenjs");
+
+var server=http.createServer(function(req,res){
+    var pathname=url.parse(req.url).pathname;
+	var fsCallback = function(error, data) {
+        if(error) throw error;
+
+        res.writeHead(200);
+        res.write(data);
+        res.end();
+    }
+    switch(pathname){
+        case '/pc':
+            doc = fs.readFile(__dirname + '/server-minified.html', fsCallback);
+        break;
+		case '/server-dev':
+            doc = fs.readFile(__dirname + '/server-dev.html', fsCallback);
+        break;
+		case '/server':
+            doc = fs.readFile(__dirname + '/server-source.html', fsCallback);
+        break;
+		case '/phone':
+            doc = fs.readFile(__dirname + '/client.html', fsCallback);
+        break;
+		case '/client-dev':
+            doc = fs.readFile(__dirname + '/client-dev.html', fsCallback);
+        break;
+		case '/client':
+            doc = fs.readFile(__dirname + '/client-source.html', fsCallback);
+        break;
+		case '/sync':
+            doc = fs.readFile(__dirname + '/sync.html', fsCallback);
+        break;
+        default:
+            doc = fs.readFile(__dirname + '/index.html', fsCallback);
+        break;
+    }
+	
+}).listen(80);
+
+console.log("InstantPhoto had started!");
+
+// Initialize Socket.io 
+var io = require('socket.io').listen(server);
+
+// Register "connection" events to the WebSocket
+io.sockets.on("connection", function(socket) {
+  // Register "join" events, requested by a connected client
+  socket.on("join", function (room) {
+    // join channel provided by client
+	socket.room = room;
+    socket.join(room);
+	// Register "image" events, sent by the client
+	socket.on("image", function(msg , rrr) {
+     // Broadcast the "image" event to all other clients in the room
+     socket.to(rrr).emit("image", msg, rrr);
+    });
+  })
+  
+  // Register "leave" events, sent by the client
+  socket.on("leave", function(room) {
+	// leave the current room  
+	socket.leave(room);
+  });
+  
+  // Register "switchroom" events, sent by development version of pc side
+  socket.on("switchroom", function(newroom) {
+	// leave the current room (stored in session)
+	socket.leave(socket.room);
+	// join new room, received as function parameter
+	socket.join(newroom);
+	});
+  });
+  
+
+
+var os = require( 'os' );
+var networkInterfaces = os.networkInterfaces( );
+var arr = networkInterfaces['Ethernet']
+var ip = arr[1].address;
+console.log("Server IP:",ip);
+
+var opn = require('opn');
+// specify the app to open in 
+opn('http://localhost', {app: 'chrome'});
